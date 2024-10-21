8de2887260e18157e23d93105b0b0b2dcf090a92
Jakub Chojnacki <jakub.chojnacki27@gmail.com>
2024-10-21 20:53:28 +0200

    added an option to host a local server that lists your notes

--- src/command.js
+++ src/command.js
@@ -1,6 +1,7 @@
 import yargs from "yargs";
 import { hideBin } from "yargs/helpers";
 import { findNotes, newNote, getAllNotes, removeAllNotes } from "./notes.js";
+import { start } from "./server.js";
 
 const listNotes = (notes) => {
   notes.forEach(({ id, content, tags }) => {
@@ -88,7 +89,10 @@ yargs(hideBin(process.argv))
         type: "number",
       });
     },
-    async (argv) => {}
+    async (argv) => {
+      const notes = await getAllNotes();
+      start(notes, argv.port);
+    }
   )
   .command(
     "clean",
